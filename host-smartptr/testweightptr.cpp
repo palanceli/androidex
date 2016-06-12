cdf0673edaf80ee95488b1f4634dabc57e07e9da
palance <palanceli@163.com>
2016-06-13 01:01:00 +0800

    commit from home

--- host-smartptr/testweightptr.cpp
+++ host-smartptr/testweightptr.cpp
@@ -17,93 +17,93 @@ void WeightClass::printRefCount()
 	Logging("StrongCount=%d. WeakCount=%d.", (strong == INITIAL_STRONG_VALUE ? 0 :strong), ref->getWeakCount());
 }
 
+// // ---------------------------------------------------------------------------
+
+// class StrongClass :public WeightClass
+// {
+// public:
+// 	StrongClass(){Logging("StrongClass::StrongClass()");}
+// 	virtual ~StrongClass(){Logging("StrongClass::~StrongClass()");}
+// };
+
+
+// // ---------------------------------------------------------------------------
+// class WeakClass : public WeightClass
+// {
+// public:
+// 	WeakClass(){Logging("WeakClass::WeakClass()");}
+// 	virtual ~WeakClass(){Logging("WeakClass::~WeakClass()");}
+// };
+// // ---------------------------------------------------------------------------
+
+// class ForeverClass : public WeightClass
+// {
+// public:
+// 	ForeverClass(){Logging("ForeverClass::ForeverClass()");}
+// 	virtual ~ForeverClass(){Logging("ForeverClass::~ForeverClass()");}
+// };
 // ---------------------------------------------------------------------------
 
-class StrongClass :public WeightClass
-{
-public:
-	StrongClass(){Logging("StrongClass::StrongClass()");}
-	virtual ~StrongClass(){Logging("StrongClass::~StrongClass()");}
-};
+// void TestStrongClass(StrongClass* pStrongClass)
+// {
+// 	wp<StrongClass> wpOut = pStrongClass;
+// 	pStrongClass->printRefCount();
+// 	{
+// 		sp<StrongClass> spInner = pStrongClass;
+// 		pStrongClass->printRefCount();;
+// 	}
+// 	sp<StrongClass> spOut = wpOut.promote();
+// 	Logging("spOut: %p.", spOut.get());
+// }
 
+// void TestWeakClass(WeakClass* pWeakClass)
+// {
+// 	wp<WeakClass> wpOut = pWeakClass;
+// 	pWeakClass->printRefCount();
+// 	{
+// 		sp<WeakClass> spInner = pWeakClass;
+// 		pWeakClass->printRefCount();
+// 	}
+// 	pWeakClass->printRefCount();
+// 	sp<WeakClass> spOut = wpOut.promote();
+// 	Logging("spOut: %p.", spOut.get());
+// }
 
-// ---------------------------------------------------------------------------
-class WeakClass : public WeightClass
-{
-public:
-	WeakClass(){Logging("WeakClass::WeakClass()");}
-	virtual ~WeakClass(){Logging("WeakClass::~WeakClass()");}
-};
-// ---------------------------------------------------------------------------
-
-class ForeverClass : public WeightClass
-{
-public:
-	ForeverClass(){Logging("ForeverClass::ForeverClass()");}
-	virtual ~ForeverClass(){Logging("ForeverClass::~ForeverClass()");}
-};
-// ---------------------------------------------------------------------------
-
-void TestStrongClass(StrongClass* pStrongClass)
-{
-	wp<StrongClass> wpOut = pStrongClass;
-	pStrongClass->printRefCount();
-	{
-		sp<StrongClass> spInner = pStrongClass;
-		pStrongClass->printRefCount();;
-	}
-	sp<StrongClass> spOut = wpOut.promote();
-	Logging("spOut: %p.", spOut.get());
-}
-
-void TestWeakClass(WeakClass* pWeakClass)
-{
-	wp<WeakClass> wpOut = pWeakClass;
-	pWeakClass->printRefCount();
-	{
-		sp<WeakClass> spInner = pWeakClass;
-		pWeakClass->printRefCount();
-	}
-	pWeakClass->printRefCount();
-	sp<WeakClass> spOut = wpOut.promote();
-	Logging("spOut: %p.", spOut.get());
-}
-
-void TestForeverClass(ForeverClass* pForeverClass)
-{
-	wp<ForeverClass> wpOut = pForeverClass;
-	pForeverClass->printRefCount();
-	{
-		sp<ForeverClass> spInner = pForeverClass;
-		pForeverClass->printRefCount();
-	}
-}
+// void TestForeverClass(ForeverClass* pForeverClass)
+// {
+// 	wp<ForeverClass> wpOut = pForeverClass;
+// 	pForeverClass->printRefCount();
+// 	{
+// 		sp<ForeverClass> spInner = pForeverClass;
+// 		pForeverClass->printRefCount();
+// 	}
+// }
 
 int testSWPtr(int argc, char const * argv[])
 {
 	WeightClass* pObj = new WeightClass();
 	sp<WeightClass> spObj(pObj);
 	spObj->printRefCount();
-	wp<WeightClass> wpObj(pObj);
+	wp<WeightClass> wpObj(spObj);
 	spObj->printRefCount();
 	return 0;
 }
 
-int testweightptr(int argc, char const * argv[])
-{
-	Logging("Test Strong Class:");
-	StrongClass* pStrongClass = new StrongClass();
-	TestStrongClass(pStrongClass);
+// int testweightptr(int argc, char const * argv[])
+// {
+// 	Logging("Test Strong Class:");
+// 	StrongClass* pStrongClass = new StrongClass();
+// 	TestStrongClass(pStrongClass);
 
-	Logging("Test Weak Class:");
-	WeakClass* pWeakClass = new WeakClass();
-	TestWeakClass(pWeakClass);
+// 	Logging("Test Weak Class:");
+// 	WeakClass* pWeakClass = new WeakClass();
+// 	TestWeakClass(pWeakClass);
 
-	Logging("Test Forever Class:");
-	ForeverClass* pForeverClass = new ForeverClass();
-	TestForeverClass(pForeverClass);
-	pForeverClass->printRefCount();
-	delete pForeverClass;
+// 	Logging("Test Forever Class:");
+// 	ForeverClass* pForeverClass = new ForeverClass();
+// 	TestForeverClass(pForeverClass);
+// 	pForeverClass->printRefCount();
+// 	delete pForeverClass;
 
-	return 0;
-}
+// 	return 0;
+// }
