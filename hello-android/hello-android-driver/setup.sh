3c57d7fdbbcd0d2cdf0bfcbcc920685827e9da99
palance <palanceli@163.com>
2016-08-15 20:24:19 +0800

    commit hello-android/setup.sh

---
+++ hello-android/hello-android-driver/setup.sh
@@ -0,0 +1,5 @@
+scriptPath=$(cd "$(dirname "$0")"; pwd)
+if [ ! -L "$scriptPath/../../../android-6.0.1_r11/kernel/goldfish/drivers/hello-android" ]; then
+	echo "创建软链接 android-6.0.1_r11/kernel/goldfish/drivers/hello-android --> $scriptPath"
+	ln -s $scriptPath $scriptPath/../../../android-6.0.1_r11/kernel/goldfish/drivers/hello-android
+fi
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-sh drivers-freg/setup.sh
+#sh drivers-freg/setup.sh
 #sh external-freg/setup.sh
 #sh external-testlog/setup.sh
 #sh libhardware-freg/setup.sh
 #sh external-helloworld/setup.sh
-sh external-testservice/setup.sh
+#sh external-testservice/setup.sh
+sh hello-android/hello-android-driver/setup.sh
